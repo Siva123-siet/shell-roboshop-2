3f2fc39dcdab9bb65db95f137c5fe31d97f12997
Manjula123-hi <danthulurimanjula@gmail.com>
2025-05-25 16:29:10 +0530

    commited

--- catalogue.sh
+++ catalogue.sh
@@ -17,8 +17,14 @@ VALIDATE $? "Copying mongo repo to the path"
 dnf install mongodb-mongosh -y &>> $LOG_FILE
 VALIDATE $? "Installing mongodb"
 
-mongosh --host mongodb.daws-84s.store </app/db/master-data.js &>> $LOG_FILE
-VALIDATE $? "Loading data to mongodb server"
+STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading data into MongoDB"
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi
 
 print_time
 
